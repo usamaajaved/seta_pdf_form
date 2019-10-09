c2b54db0a0275bbbfb63158c329cf754a1b83979
Usama Javed <46658189+usamaajaved@users.noreply.github.com>
2019-10-09 18:37:42 +0500

    commit

--- login.php
+++ login.php
@@ -3,66 +3,54 @@
 <div class="page-content">
 
 		<!-- Main content -->
-		<div class="content-wrapper">
+	<div class="content-wrapper">
 
-			<!-- Content area -->
-			<div class="content d-flex justify-content-center align-items-center">
+		<!-- Content area -->
+		<div class="content d-flex justify-content-center align-items-center">
 
-				<!-- Login card -->
-				<form class="login-form" action="index.html">
-					<div class="card mb-0">
-						<div class="card-body">
-							<div class="text-center mb-3">
-								<i class="icon-reading icon-2x text-slate-300 border-slate-300 border-3 rounded-round p-3 mb-3 mt-1"></i>
-								<h5 class="mb-0">Login to your account</h5>
-								<span class="d-block text-muted">Your credentials</span>
-							</div>
+			<!-- Login card -->
+			<form class="login-form" action="index.html">
+				<div class="card mb-0">
+					<div class="card-body">
+						<div class="text-center mb-3">
+							<i class="icon-reading icon-2x text-slate-300 border-slate-300 border-3 rounded-round p-3 mb-3 mt-1"></i>
+							<h5 class="mb-0">Login to your account</h5>
+						</div>
 
-							<div class="form-group form-group-feedback form-group-feedback-left">
-								<input type="text" class="form-control" placeholder="Username">
-								<div class="form-control-feedback">
-									<i class="icon-user text-muted"></i>
-								</div>
-							</div>
-
-							<div class="form-group form-group-feedback form-group-feedback-left">
-								<input type="password" class="form-control" placeholder="Password">
-								<div class="form-control-feedback">
-									<i class="icon-lock2 text-muted"></i>
-								</div>
-							</div>
-
-							<div class="form-group d-flex align-items-center">
-								<div class="form-check mb-0">
-									<label class="form-check-label">
-										<input type="checkbox" name="remember" class="form-input-styled" checked data-fouc>
-										Remember
-									</label>
-								</div>
-
-								<a href="login_password_recover.html" class="ml-auto">Forgot password?</a>
-							</div>
-
-							<div class="form-group">
-								<button type="submit" class="btn btn-primary btn-block">Sign in <i class="icon-circle-right2 ml-2"></i></button>
-							</div>
-
-							<div class="form-group text-center text-muted content-divider">
-								<span class="px-2">Don't have an account?</span>
-							</div>
-
-							<div class="form-group">
-								<a href="#" class="btn btn-light btn-block">Sign up</a>
+						<div class="form-group form-group-feedback form-group-feedback-left">
+							<input type="text" class="form-control" placeholder="Username">
+							<div class="form-control-feedback">
+								<i class="icon-user text-muted"></i>
 							</div>
 						</div>
-					</div>
-				</form>
-				<!-- /login card -->
 
-			</div>
-			<!-- /content area -->
+						<div class="form-group form-group-feedback form-group-feedback-left">
+							<input type="password" class="form-control" placeholder="Password">
+							<div class="form-control-feedback">
+								<i class="icon-lock2 text-muted"></i>
+							</div>
+						</div>
+
+						<div class="form-group d-flex align-items-center">
+							<a href="login_password_recover.html" class="ml-auto">Forgot password?</a>
+						</div>
+
+						<div class="form-group">
+							<button type="submit" class="btn btn-primary btn-block">Sign in <i class="icon-circle-right2 ml-2"></i></button>
+						</div>
+
+						<div class="form-group">
+							<a href="http://localhost/seta_pdf_form/register.php" class="btn btn-light btn-block">Sign up</a>
+						</div>
+					</div>
+				</div>
+			</form>
+			<!-- /login card -->
 
 		</div>
-		<!-- /main content -->
+		<!-- /content area -->
 
 	</div>
+	<!-- /main content -->
+
+</div>
