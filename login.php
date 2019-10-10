7ee10a1035f7e4c094ca65553aa922ffeda3471f
zubair042 <042zubair@.github.com>
2019-10-10 19:43:48 +0500

    commit

--- login.php
+++ login.php
@@ -47,7 +47,7 @@
 						</div>
 
 						<div class="form-group">
-							<a href="<?php echo base_url; ?>register.php" class="btn btn-light btn-block">Sign up</a>
+							<a href="register.php" class="btn btn-light btn-block">Sign up</a>
 						</div>
 					</div>
 				</div>
