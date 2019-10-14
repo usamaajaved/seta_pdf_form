64ad307010e83c704a4ea19ed99060712e2e249d
Usama Javed <46658189+usamaajaved@users.noreply.github.com>
2019-10-14 11:34:35 +0500

    commit

--- authentication/reset_password.php
+++ authentication/reset_password.php
@@ -1,7 +1,7 @@
 <?php
 include "../config/config.php";
 
-print_r($_POST);
+//print_r($_POST);
 
 $new_pass = md5($_POST['new_password']);
 $confirm_new_pass = md5($_POST['confirm_password']);
@@ -9,8 +9,9 @@ $confirm_new_pass = md5($_POST['confirm_password']);
 if ($new_pass != $confirm_new_pass) {
 	echo "Password dosent match";
 } elseif ($new_pass == $confirm_new_pass) {
-	$res = 'UPDATE users SET `password` = '.$confirm_new_pass.' WHERE `id` = 2'; 
+	$res = 'UPDATE users SET `password` = "'.$confirm_new_pass.'" WHERE `id` = '.$_POST['user_id'].' '; 
 	mysqli_query($con_str,$res);
+	echo "password changed";
 	//header('Location: login.php');
 } else {
 	echo "string";
